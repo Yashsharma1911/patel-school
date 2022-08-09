42763240ef91ebdc9b1ee70e10dca1e8d008a90d
yash sharma <yashsharma2572@gmail.com>
2022-08-09 16:48:04 +0530

    Edited attention line and added fade animation

--- src/components/attentionLine/styles/attention.js
+++ src/components/attentionLine/styles/attention.js
@@ -1,19 +1,12 @@
 import styled from "styled-components";
+export const SpanText = styled.span`
+font-style: normal;
+font-weight: bold;
+font-size: 96px;
+line-height: 116%;
 
-export const Container = styled.div`
-    margin-top: 75px;
-    width: 100%;
-    @media (max-width: 1000px) {
-        margin-top: 30px;
-    }
-`
-export const Text = styled.p`
-    font-style: normal;
-    font-weight: bold;
-    font-size: 96px;
-    line-height: 116%;
-    color: #000000;
-    text-align: center;
+text-align: center;
+    color: #D43260;
 
     @media (min-width: 1899px) {
         font-size: 126px;
@@ -34,9 +27,55 @@ export const Text = styled.p`
         font-size: 56px;
     }
 `;
-export const SpanText = styled.span`
+
+export const Text = styled.p`
+display: inline-block;
     font-style: normal;
     font-weight: bold;
+    font-size: 96px;
     line-height: 116%;
-    color: #D43260;
+    color: #000000;
+    text-align: center;
+    transition: all 0.3s ease-in-out;
+
+    @media (min-width: 1899px) {
+        font-size: 126px;
+    }
+    @media (max-width: 1420px) {
+        font-size: 86px;
+    }
+    @media (max-width: 1270px) {
+        font-size: 76px;
+    }
+    @media (max-width: 1120px) {
+        font-size: 66px;
+    }
+    @media (max-width: 975px) {
+        text-align: left;
+    }
+    @media (max-width: 368px) {
+        font-size: 56px;
+    }
 `;
+
+export const Container = styled.div`
+    margin-top: 75px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    &:hover {
+        ${Text}{
+            opacity: 0.3;
+        }
+    }
+
+    @media (max-width: 1000px) {
+        margin-top: 30px;
+    }
+    @media (max-width: 550px) {
+        justify-content: start;
+    }
+`
+
