eed819fe8d529ee5e307617cc30e496f50e75414
yash sharma <yashsharma2572@gmail.com>
2022-08-12 02:05:48 +0530

    🥑 added use-auth-listener hook to check whether user logedIn or not

---
+++ src/hooks/use-auth-listener.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../lib/firebase';
+
+export default function UseAuthListener() {
+    const [user, setUser] = useState(null);
+    useEffect(() => {
+        const listener = onAuthStateChanged(auth, (User) => {
+            if (User) {
+                setUser(User);
+            } else {
+                setUser(null);
+            }
+        });
+        return () => listener();
+    }, []);
+    return { user };
+}
