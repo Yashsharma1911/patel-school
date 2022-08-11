b0f491db8b032f305b07b453fb9b7b1eb4c18ad1
yash sharma <yashsharma2572@gmail.com>
2022-08-11 16:46:29 +0530

    deleted route configuration from app.js because route file is seprate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
-import { Home, Signup, Signin, GalleryImage, Courses, Result, Registration, Browse, Admission } from './pages';
-// import HeaderContainer from '../container/header';
-import * as ROUTES from './constants/routes'
+import Router from './routes';
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path={ROUTES.HOME} element={<Home />} />
-          <Route path={ROUTES.SIGN_UP} element={<Signup />} />
-          <Route path={ROUTES.SIGN_IN} element={<Signin />} />
-          <Route path={ROUTES.GALLERY} element={<GalleryImage />} />
-          <Route path={ROUTES.COURSES} element={<Courses />} />
-          <Route path={ROUTES.RESULT} element={<Result />} />
-          <Route path={ROUTES.REGISTRATION} element={<Registration />} />
-          <Route path={ROUTES.BROWSE} element={<Browse />} />
-          <Route path={ROUTES.ADMISSION} element={<Admission />} />
-        </Routes>
-      </Router>
+      <Router />
     </>
   );
 }
