242f83fa8c6e1846062d5f23c847696e03a62712
yash sharma <yashsharma2572@gmail.com>
2022-08-12 02:31:32 +0530

    fetched auth in lib/firebase to use useAuthListener

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,3 +17,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
+export const auth = getAuth();
