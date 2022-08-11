107fd1b43f03b98aab892973f883cb17cf66d56f
yash sharma <yashsharma2572@gmail.com>
2022-08-12 01:43:53 +0530

    🥑 created studentLogin page and added footer

--- src/pages/studentLogin.js
+++ src/pages/studentLogin.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import StudentLoginContainer from '../container/login/studentLogin'
+import HeaderContainer from '../container/header';
+import FooterContainer from '../container/footer';
 
 export default function StudentLogin() {
     return (
-        <div>studentLogin</div>
+        <>
+            <HeaderContainer />
+            <StudentLoginContainer />
+            <FooterContainer />
+        </>
     )
 }
