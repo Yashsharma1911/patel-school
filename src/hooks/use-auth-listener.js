ade05b984f9923c57664575aa9e06f53e3635856
yash sharma <yashsharma2572@gmail.com>
2022-08-12 02:29:29 +0530

    🥑 created custom hook to check user

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../lib/firebase';
+import { auth } from '../lib/firebase.prod.js';
 
 export default function UseAuthListener() {
     const [user, setUser] = useState(null);
