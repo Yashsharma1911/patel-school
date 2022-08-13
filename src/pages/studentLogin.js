75582ee01952b18dee942ea0a97cc4022b285952
yash sharma <yashsharma2572@gmail.com>
2022-08-13 11:01:55 +0530

    🥑 Added resetpassword component

--- src/pages/studentLogin.js
+++ src/pages/studentLogin.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import StudentLoginContainer from '../container/login/studentLogin'
 import HeaderContainer from '../container/header';
 import FooterContainer from '../container/footer';
+import ResetPasswordContainer from '../container/login/resetPassword';
 
 export default function StudentLogin() {
+    const [reset, setReset] = useState(false);
     return (
         <>
             <HeaderContainer />
-            <StudentLoginContainer />
+            {reset ?
+                <ResetPasswordContainer />
+                :
+                <StudentLoginContainer setReset={setReset} />
+            }
             <FooterContainer />
         </>
     )
