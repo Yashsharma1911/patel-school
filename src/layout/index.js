5be8ad81b129b49af2bbb4407d7d40b71bf2fe67
yash sharma <yashsharma2572@gmail.com>
2022-08-13 10:53:42 +0530

    🛠  Created layout for dashboard

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import DashboardSidebar from './dashboardSidebar'
+import DashboardNavbar from './dashboardNavbar'
+import styled from 'styled-components'
+
+// React Router
+import { Outlet } from 'react-router-dom'
+
+
+const RootStyle = styled('div')({
+    display: 'flex',
+    minHeight: '100%',
+    overflow: 'hidden'
+});
+
+const MainStyle = styled('div')({
+    width: "100%",
+});
 
 export default function DashboardLayout() {
     return (
-        <div>index</div>
+        <>
+            <DashboardNavbar />
+            <RootStyle>
+                <DashboardSidebar />
+                <MainStyle>
+                    <Outlet />
+                </MainStyle>
+            </RootStyle>
+        </>
     )
 }
