6836f97593c9b9b65e60e6ab93e9da97c9163610
yash sharma <yashsharma2572@gmail.com>
2022-08-13 10:55:59 +0530

    🥑 Added necessary setup of baseUI

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import App from './App';
 import GlobalStyles from './global-styles';
 import { BrowserRouter } from 'react-router-dom';
 
+// baseweb ui
+import { BaseProvider, LightTheme } from 'baseui';
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+const engine = new Styletron();
+
 ReactDOM.render(
   <>
     <BrowserRouter>
       <GlobalStyles />
-      <App />
+      <StyletronProvider value={engine}>
+        <BaseProvider theme={LightTheme}>
+          <App />
+        </BaseProvider>
+      </StyletronProvider>
     </BrowserRouter>
   </>,
   document.getElementById('root'),
